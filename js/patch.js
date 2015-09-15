1fb4298327f8b49f2d2e12f90f1b2f6d35d006c7
Neon4o4 <baiyinshuo@clatter.cn>
2015-09-15 23:34:57 +0800

    Sidebar on the left is now working.

--- js/patch.js
+++ js/patch.js
@@ -1,20 +1,20 @@
 // alert("It works");
 if(document.getElementsByName("userpwd_text")[0] !== undefined){
-document.getElementsByName("userpwd_text")[0].id = "userpwd_text";
+    document.getElementsByName("userpwd_text")[0].id = "userpwd_text";
 }
 
-window.onload  = function (){
-
-var t = 110;
-var aa = window.top.document.getElementsByName("leftFrame")[0].contentDocument;
-if (aa !== undefined){
-for (var i = 0; i < 21; i++) {
-   var tmp = aa.getElementById('MFX' + i);
-   if (tmp){
-    tmp.style["visibility"]= 'visible';
-    tmp.style["top"] = t + 'px';
-    t = t + 12;
-   }
-}
-}
+// slidebar on the left
+var contnt = window.top.document.getElementsByName("leftFrame")[0].contentDocument;
+if (contnt !== undefined){
+    var topLine = contnt.getElementById("MFX0").style["top"];
+    var attrHeight = 12
+    topLine = parseInt(topLine);
+    for (var i = 0; i < 21; i++) {
+        var MFX = contnt.getElementById("MFX" + i);
+        if (MFX){
+            var attrTop = topLine + i * attrHeight;
+            MFX.style["visibility"]= "visible";
+            MFX.style["top"] = attrTop.toString() + "px";
+        }
+    }
 }
